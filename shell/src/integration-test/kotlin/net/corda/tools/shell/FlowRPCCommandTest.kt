2232c5c72abbe147024d4fd6b1024399d96ab940
Arshad Mahmood <1391251+arshadm@users.noreply.github.com>
2023-10-03 17:32:21 +0100

    wip

--- shell/src/integration-test/kotlin/net/corda/tools/shell/FlowRPCCommandTest.kt
+++ shell/src/integration-test/kotlin/net/corda/tools/shell/FlowRPCCommandTest.kt
@@ -5,9 +5,11 @@ import net.corda.core.contracts.PartyAndReference
 import net.corda.core.contracts.StateAndRef
 import net.corda.core.crypto.SecureHash
 import net.corda.core.flows.*
+import net.corda.core.flows.DistributionList.SenderDistributionList
 import net.corda.core.identity.Party
 import net.corda.core.messaging.startFlow
 import net.corda.core.messaging.startTrackedFlow
+import net.corda.core.node.StatesToRecord
 import net.corda.core.transactions.SignedTransaction
 import net.corda.core.utilities.OpaqueBytes
 import net.corda.core.utilities.getOrThrow
@@ -23,7 +25,6 @@ import net.corda.testing.driver.driver
 import net.corda.testing.node.User
 import net.corda.testing.node.internal.cordappWithPackages
 import net.corda.testing.node.internal.findCordapp
-import org.junit.Ignore
 import org.junit.Test
 import java.time.Duration
 import java.util.*
@@ -55,8 +56,16 @@ class FlowRPCCommandTest : CommandTestBase() {
             assertFailsWith<TimeoutException> { handle.returnValue.getOrThrow(timeout = Duration.ofSeconds(30)) }
             assertEquals(1, timesThrown)
             val session = connectToShell(user, node)
-            testCommand(session, command = "flowStatus queryById ${id.uuid.toString().toLowerCase()}", expected = "HOSPITALIZED")
-            testCommand(session, command = "flow retry ${id.uuid.toString().toLowerCase()}", expected = "Retried flow $id")
+            testCommand(
+                session,
+                command = "flowStatus queryById ${id.uuid.toString().toLowerCase()}",
+                expected = "HOSPITALIZED"
+            )
+            testCommand(
+                session,
+                command = "flow retry ${id.uuid.toString().toLowerCase()}",
+                expected = "Retried flow $id"
+            )
             assertFailsWith<TimeoutException> { handle.returnValue.getOrThrow(timeout = Duration.ofSeconds(10)) }
             assertEquals(2, timesThrown)
             session.disconnect()
@@ -67,35 +76,51 @@ class FlowRPCCommandTest : CommandTestBase() {
     fun `A flow can be paused and resumed via the shell`() {
         val user = User("username", "password", setOf(Permissions.all()))
         driver(DriverParameters(notarySpecs = emptyList(), cordappsForAllNodes = listOf(customCordapp))) {
-            val node = startNode(providedName = ALICE_NAME, rpcUsers = listOf(user), startInSameProcess = true).getOrThrow()
+            val node =
+                startNode(providedName = ALICE_NAME, rpcUsers = listOf(user), startInSameProcess = true).getOrThrow()
             PauseFlow.beforePause = Semaphore(0)
             val handle = node.rpc.startFlow(::PauseFlow)
             val id = handle.id
             val session = connectToShell(user, node)
-            testCommand(session, command = "flow pause ${id.uuid.toString().toLowerCase()}", expected = "Paused flow $id")
+            testCommand(
+                session,
+                command = "flow pause ${id.uuid.toString().toLowerCase()}",
+                expected = "Paused flow $id"
+            )
             PauseFlow.beforePause!!.release()
             Thread.sleep(5000) //Wait for the Statemachine to go through a pause transition.
-            testCommand(session, command = "flowStatus queryById ${id.uuid.toString().toLowerCase()}", expected = "PAUSED")
-            testCommand(session, command = "flow retry ${id.uuid.toString().toLowerCase()}", expected = "Retried flow $id")
+            testCommand(
+                session,
+                command = "flowStatus queryById ${id.uuid.toString().toLowerCase()}",
+                expected = "PAUSED"
+            )
+            testCommand(
+                session,
+                command = "flow retry ${id.uuid.toString().toLowerCase()}",
+                expected = "Retried flow $id"
+            )
             handle.returnValue.getOrThrow()
         }
     }
 
     @Test(timeout = 300_000)
-    @Ignore("TODO JDK17:Fixme")
     fun `A finality flow can be recovered via the shell`() {
         val user = User("username", "password", setOf(Permissions.all()))
-        driver(DriverParameters(cordappsForAllNodes = listOf(customCordapp, findCordapp("net.corda.testing.contracts"))
-        )) {
-            val alice = startNode(providedName = ALICE_NAME, rpcUsers = listOf(user), startInSameProcess = true).getOrThrow()
-            val bob = startNode(providedName = BOB_NAME, rpcUsers = listOf(user), startInSameProcess = true).getOrThrow()
+        driver(
+            DriverParameters(
+                cordappsForAllNodes = listOf(customCordapp, findCordapp("net.corda.testing.contracts"))
+            )
+        ) {
+            val alice =
+                startNode(providedName = ALICE_NAME, rpcUsers = listOf(user), startInSameProcess = true).getOrThrow()
+            val bob =
+                startNode(providedName = BOB_NAME, rpcUsers = listOf(user), startInSameProcess = true).getOrThrow()
             val stateAndRef = alice.rpc.startFlow(::IssueFlow, defaultNotaryIdentity).returnValue.getOrThrow()
             val handle = alice.rpc.startFlow(::TransferFlow, stateAndRef, bob.nodeInfo.legalIdentities.first())
             try {
                 handle.returnValue.getOrThrow()
                 fail()
-            }
-            catch (e: FinalityFlowException) {
+            } catch (e: FinalityFlowException) {
                 println("FinalityFlowException: $${e.message}")
                 val session = connectToShell(user, alice)
                 testCommand(
@@ -145,17 +170,30 @@ class FlowRPCCommandTest : CommandTestBase() {
 
     @InitiatingFlow
     @StartableByRPC
-    class TransferFlow(private val stateAndRef: StateAndRef<DummyContract.SingleOwnerState>, private val newOwner: Party) : FlowLogic<SignedTransaction>() {
+    class TransferFlow(
+        private val stateAndRef: StateAndRef<DummyContract.SingleOwnerState>,
+        private val newOwner: Party
+    ) : FlowLogic<SignedTransaction>() {
         @Suspendable
         override fun call(): SignedTransaction {
             val txBuilder = DummyContract.move(stateAndRef, newOwner)
             val signedTransaction = serviceHub.signInitialTransaction(txBuilder, ourIdentity.owningKey)
             (serviceHub as ServiceHubInternal).recordUnnotarisedTransaction(signedTransaction)
+            (serviceHub as ServiceHubInternal).recordSenderTransactionRecoveryMetadata(
+                signedTransaction.id,
+                TransactionMetadata(
+                    initiator = ourIdentity.name,
+                    distributionList = SenderDistributionList(
+                        senderStatesToRecord = StatesToRecord.ONLY_RELEVANT,
+                        peersToStatesToRecord = mapOf(newOwner.name to StatesToRecord.ONLY_RELEVANT)
+                    )
+                )
+            )
             sleep(Duration.ZERO)    // force checkpoint / txn commit
             throw FinalityFlowException(signedTransaction.id, runId.uuid)
         }
     }
 
-    class FinalityFlowException(val txnId: SecureHash, val flowId: UUID)
-        : FlowException("Finality flow $flowId failed for transaction id: $txnId")
+    class FinalityFlowException(val txnId: SecureHash, val flowId: UUID) :
+        FlowException("Finality flow $flowId failed for transaction id: $txnId")
 }
