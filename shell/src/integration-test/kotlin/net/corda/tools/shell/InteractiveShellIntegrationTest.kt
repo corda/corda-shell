74f00ba1432d713dc7bb7de1e8d8e55d7db36b4f
Arshad Mahmood <1391251+arshadm@users.noreply.github.com>
2023-10-09 17:09:16 +0100

    enable shell integration tests

--- shell/src/integration-test/kotlin/net/corda/tools/shell/InteractiveShellIntegrationTest.kt
+++ shell/src/integration-test/kotlin/net/corda/tools/shell/InteractiveShellIntegrationTest.kt
@@ -5,9 +5,9 @@ import com.fasterxml.jackson.databind.ObjectMapper
 import com.fasterxml.jackson.databind.type.TypeFactory
 import com.jcraft.jsch.ChannelExec
 import com.jcraft.jsch.JSch
-import com.nhaarman.mockito_kotlin.any
-import com.nhaarman.mockito_kotlin.doAnswer
-import com.nhaarman.mockito_kotlin.mock
+import org.mockito.kotlin.any
+import org.mockito.kotlin.doAnswer
+import org.mockito.kotlin.mock
 import net.corda.client.jackson.JacksonSupport
 import net.corda.client.jackson.internal.valueAs
 import net.corda.client.rpc.RPCException
@@ -52,7 +52,6 @@ import net.corda.testing.driver.internal.checkpoint.CheckpointRpcHelper.checkpoi
 import net.corda.testing.internal.useSslRpcOverrides
 import net.corda.testing.node.User
 import net.corda.testing.node.internal.enclosedCordapp
-import net.corda.tools.shell.SSHServerTest.FlowICanRun
 import net.corda.tools.shell.utlities.ANSIProgressRenderer
 import org.apache.activemq.artemis.api.core.ActiveMQSecurityException
 import org.assertj.core.api.Assertions.assertThat
@@ -60,7 +59,6 @@ import org.assertj.core.api.Assertions.assertThatThrownBy
 import org.bouncycastle.util.io.Streams
 import org.crsh.text.RenderPrintWriter
 import org.junit.Before
-import org.junit.Ignore
 import org.junit.Rule
 import org.junit.Test
 import org.junit.jupiter.api.assertThrows
@@ -156,7 +154,6 @@ class InteractiveShellIntegrationTest {
         }
     }
 
-    @Ignore
     @Test(timeout = 300_000)
     fun `ssh runs flows via standalone shell`() {
         val user = User(
@@ -185,18 +182,18 @@ class InteractiveShellIntegrationTest {
             assertTrue(channel.isConnected)
 
             val response = String(Streams.readAll(channel.inputStream))
+            val lines = response.lines()
 
-            val linesWithDoneCount = response.lines().filter { line -> "Done" in line }
+            val linesWithDoneCount = lines.filter { line -> "Done" in line }
 
             channel.disconnect()
             session.disconnect()
 
             // There are ANSI control characters involved, so we want to avoid direct byte to byte matching.
-            assertThat(linesWithDoneCount).hasSize(1)
+            assertThat(linesWithDoneCount).hasSize(3)
         }
     }
 
-    @Ignore
     @Test(timeout = 300_000)
     fun `ssh run flows via standalone shell over ssl to node`() {
         val user = User(
@@ -240,7 +237,7 @@ class InteractiveShellIntegrationTest {
                 session.disconnect() // TODO Simon make sure to close them
 
                 // There are ANSI control characters involved, so we want to avoid direct byte to byte matching.
-                assertThat(linesWithDoneCount).hasSize(1)
+                assertThat(linesWithDoneCount).hasSize(3)
 
                 successful = true
             }
@@ -587,5 +584,20 @@ class InteractiveShellIntegrationTest {
         override fun call() {
             waitForStateConsumption(stateRefs)
         }
+    }
+
+    @StartableByRPC
+    @InitiatingFlow
+    class FlowICanRun : FlowLogic<String>() {
+
+        private val HELLO_STEP = ProgressTracker.Step("Hello")
+
+        @Suspendable
+        override fun call(): String {
+            progressTracker?.currentStep = HELLO_STEP
+            return "bambam"
+        }
+
+        override val progressTracker: ProgressTracker? = ProgressTracker(HELLO_STEP)
     }
 }
